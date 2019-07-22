bb4c0dc1b153557ddfaa69de4b268a45abba2274
Iyay <rtriharyana@gmail.com>
2019-07-22 20:42:11 +0700

    Workaround fix i18n.languages were undefined or empty undefined

--- src/create-i18next-client.js
+++ src/create-i18next-client.js
@@ -1,34 +1,37 @@
-import isNode from 'detect-node'
-import i18next from 'i18next'
-import i18nextXHRBackend from 'i18next-xhr-backend'
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+import isNode from 'detect-node';
+import i18next from 'i18next';
+import i18nextXHRBackend from 'i18next-xhr-backend';
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 
-const i18n = i18next.default ? i18next.default : i18next
+const i18n = i18next.default ? i18next.default : i18next;
 
-export default (config) => {
+export default config => {
   if (!i18n.isInitialized) {
-
     if (isNode) {
-      const i18nextNodeBackend = eval("require('i18next-node-fs-backend')")
-      const i18nextMiddleware = eval("require('i18next-express-middleware')")
-      i18n.use(i18nextNodeBackend)
+      const i18nextNodeBackend = eval("require('i18next-node-fs-backend')");
+      const i18nextMiddleware = eval("require('i18next-express-middleware')");
+      i18n.use(i18nextNodeBackend);
       if (config.serverLanguageDetection) {
-        const serverDetectors = new i18nextMiddleware.LanguageDetector()
-        config.customDetectors.forEach(detector => serverDetectors.addDetector(detector))
-        i18n.use(serverDetectors)
+        const serverDetectors = new i18nextMiddleware.LanguageDetector();
+        config.customDetectors.forEach(detector =>
+          serverDetectors.addDetector(detector)
+        );
+        i18n.use(serverDetectors);
       }
     } else {
-      i18n.use(i18nextXHRBackend)
+      i18n.use(i18nextXHRBackend);
       if (config.browserLanguageDetection) {
-        const browserDetectors = new I18nextBrowserLanguageDetector()
-        config.customDetectors.forEach(detector => browserDetectors.addDetector(detector))
-        i18n.use(browserDetectors)
+        const browserDetectors = new I18nextBrowserLanguageDetector();
+        config.customDetectors.forEach(detector =>
+          browserDetectors.addDetector(detector)
+        );
+        i18n.use(browserDetectors);
       }
     }
 
-    config.use.forEach(x => i18n.use(x))
-    i18n.init(config)
-
+    config.use.forEach(x => i18n.use(x));
+    i18n.languages = config.otherLanguages;
+    i18n.init(config);
   }
-  return i18n
-}
+  return i18n;
+};
